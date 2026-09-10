3202ca5f8536c4329f62a673435ff205c69a7162
felliot010560 <floyd>
2026-09-10 18:17:00 -0500

    Took out redis api call

--- src/main/java/com/aleatory/price/api/PricingController.java
+++ src/main/java/com/aleatory/price/api/PricingController.java
@@ -3,11 +3,9 @@ package com.aleatory.price.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +17,8 @@ import com.aleatory.price.services.PricingService;
 @RestController
 public class PricingController {
     private static final Logger logger = LoggerFactory.getLogger(PricingController.class);
-    private static final String REDIS_KEY = "CONDORS:LAST.MESSAGES";
     @Autowired
     private PricingService pricingService;
-    
-    @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
 
     @GetMapping("/condor")
     @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
